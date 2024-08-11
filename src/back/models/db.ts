610f29ae6fe79912e47f973a396fa1cc1ad9e0b0
CorentinTurgis <148709142+CorentinTurgis@users.noreply.github.com>
2024-08-11 22:23:43 +0200

    Add files via upload

--- src/back/models/db.ts
+++ src/back/models/db.ts
@@ -3,6 +3,6 @@ import { Connection, createConnection } from 'mysql2';
 export const db: Connection = createConnection({
   host: 'localhost',
   user: 'test',
-  password: 'root',
+  password: 'Rootroot51',
   database: 'my_calendar',
 });
